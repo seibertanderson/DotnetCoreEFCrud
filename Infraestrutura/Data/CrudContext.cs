c447e7c1648f036a135b8b46a6d39df430563f7c
Anderson Seibert <andersonseibert0@gmail.com>
2022-04-04 20:15:18 -0300

    Initial Commit

---
+++ Infraestrutura/Data/CrudContext.cs
@@ -0,0 +1,18 @@
+﻿using Infraestrutura.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infraestrutura.Data
+{
+    public class CrudContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+
+        public CrudContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ Infraestrutura/Entities/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Infraestrutura.Entities
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
